5b039a3ba85049a2cd2d93078fccae55ea63cb34
Bijeesh <bijeeshb.stackup@gmail.com>
2026-01-08 21:55:54 +0530

    dist update

--- dist/app.js
+++ dist/app.js
@@ -9,12 +9,16 @@ var cors_1 = __importDefault(require("cors")); // 1. Import CORS here
 var product_routes_1 = __importDefault(require("./routes/product.routes"));
 var offer_routes_1 = __importDefault(require("./routes/offer.routes"));
 var bill_routes_1 = __importDefault(require("./routes/bill.routes"));
+var corsOptions = {
+    // Replace this with your actual Vercel URL
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+};
 var app = (0, express_1.default)();
 // 2. Apply CORS immediately after initializing app
-app.use((0, cors_1.default)({
-    origin: "http://localhost:3000", // Be explicit about your frontend URL
-    credentials: true
-}));
+app.use((0, cors_1.default)(corsOptions));
 // 3. Body parser
 app.use(express_1.default.json());
 // 4. Routes (Must come AFTER cors)
--- dist/server.js
+++ dist/server.js
@@ -8,17 +8,6 @@ var app_1 = __importDefault(require("./app"));
 var db_1 = require("./config/db");
 var cron_1 = require("./config/cron");
 var env_1 = require("./config/env");
-var cors_1 = __importDefault(require("cors"));
-var corsOptions = {
-    // Replace this with your actual Vercel URL
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-};
-// 0. Apply CORS Middleware
-// (Important: Must be applied before routes are defined in 'app')
-app_1.default.use((0, cors_1.default)(corsOptions));
 // 1. Connect to Database
 (0, db_1.connectDB)();
 // 2. Start Background Tasks
