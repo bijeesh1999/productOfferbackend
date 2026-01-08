cde65127f3fdb12f41588df939927987b9e4ea26
Bijeesh <bijeeshb.stackup@gmail.com>
2026-01-08 21:30:06 +0530

    env update

--- src/server.ts
+++ src/server.ts
@@ -6,15 +6,22 @@ import { startCronJobs } from "./config/cron";
 import { PORT } from "./config/env";
 import cors from "cors";
 
+const corsOptions = {
+  // Replace this with your actual Vercel URL
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
 // 0. Apply CORS Middleware
 // (Important: Must be applied before routes are defined in 'app')
-app.use(cors());
+app.use(cors(corsOptions));
 // 1. Connect to Database
 connectDB();
 
 // 2. Start Background Tasks
 startCronJobs();
-
 
 // 3. Start Server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
