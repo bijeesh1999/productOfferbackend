7379a3f94d9176fa7f893ce595708968c665d099
Bijeesh <bijeeshb.stackup@gmail.com>
2026-01-08 21:31:07 +0530

    dist update

--- dist/server.js
+++ dist/server.js
@@ -9,9 +9,16 @@ var db_1 = require("./config/db");
 var cron_1 = require("./config/cron");
 var env_1 = require("./config/env");
 var cors_1 = __importDefault(require("cors"));
+var corsOptions = {
+    // Replace this with your actual Vercel URL
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+};
 // 0. Apply CORS Middleware
 // (Important: Must be applied before routes are defined in 'app')
-app_1.default.use((0, cors_1.default)());
+app_1.default.use((0, cors_1.default)(corsOptions));
 // 1. Connect to Database
 (0, db_1.connectDB)();
 // 2. Start Background Tasks
