c32b4e18866958417a92cf7ff214b0c698b65058
Bijeesh <bijeeshb.stackup@gmail.com>
2026-01-08 21:55:05 +0530

    corse error fix

--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,18 @@ import productRoutes from "./routes/product.routes";
 import offerRoutes from "./routes/offer.routes";
 import billRoutes from "./routes/bill.routes";
 
+const corsOptions = {
+  // Replace this with your actual Vercel URL
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
 const app = express();
 
 // 2. Apply CORS immediately after initializing app
-app.use(cors({
-  origin: "http://localhost:3000", // Be explicit about your frontend URL
-  credentials: true
-}));
+app.use(cors(corsOptions));
 
 // 3. Body parser
 app.use(express.json());
--- src/server.ts
+++ src/server.ts
@@ -6,17 +6,7 @@ import { startCronJobs } from "./config/cron";
 import { PORT } from "./config/env";
 import cors from "cors";
 
-const corsOptions = {
-  // Replace this with your actual Vercel URL
-  origin: process.env.FRONTEND_URL,
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
 
-// 0. Apply CORS Middleware
-// (Important: Must be applied before routes are defined in 'app')
-app.use(cors(corsOptions));
 // 1. Connect to Database
 connectDB();
 
